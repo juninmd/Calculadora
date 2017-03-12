fad1f8f25d95b9a97e411adc7320cc78bc498e8b
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-12 18:52:12 -0300

    criação das paginas

--- calculadora/src/app/app.module.ts
+++ calculadora/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize'
 import { AppComponent } from './app.component';
+import { PorcentagemComponent } from './pages/porcentagem/porcentagem.component';
+import { RelatorioComponent } from './pages/relatorio/relatorio.component';
+import { SuporteComponent } from './pages/suporte/suporte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PorcentagemComponent,
+    RelatorioComponent,
+    SuporteComponent
   ],
   imports: [
     MaterializeModule,
