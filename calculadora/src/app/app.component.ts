92fc2f5fd99405bda4f709e0e21cd9437d348150
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-12 17:51:21 -0300

    criando formulário.

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private outros: any = [];
+
+  private adicionar() {
+    this.outros.push({
+      valor: 0,
+      descricao: ''
+    });
+  }
+
+  private remover(item: any) {
+    this.outros.splice(this.outros.indexOf(item), 1);
+  }
 }
